8f4eca0faf943f92659499a6ed63d80e719b2f63
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2020-08-14 20:52:27 +0530

    Create 06.py

---
+++ 06.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+# np.zeros([height, width, 3], datatype)
+# this method will give you a black image with the following dmensions
+img = np.zeros([512, 512, 3], np.uint8)
+
+# line(image you want to load, pt 1, pt 2, (B, G, R), thickness)
+img = cv2.line(img, (0, 0), (255, 255), (0, 0, 255), 5)
+
+# arrowdLine() makes a arrowed line
+img = cv2.arrowedLine(img, (0, 255), (255, 255), (255, 0, 0), 10)
+
+# rectangle(image, pt 1(left-top vertex), pt 2(right bottom vertex), thickness)
+# to fill everything inside the rectangle use thickness as -1
+img = cv2.rectangle(img, (384, 0), (510, 128), (0, 0, 255), 10)
+
+# circle(image, center, radius, color, thickness)
+img = cv2.circle(img, (447, 63), 63, (0, 255, 0), -1)
+
+# putText(image, 'what text you want to put in the images', coordinates from where 
+# do you want to start the text, font, font size, color of the font, thickness, line type)
+font = cv2.FONT_HERSHEY_SIMPLEX
+img = cv2.putText(img, 'OpenCV', (10, 500), font, 4, (0, 255, 255), 10, cv2.LINE_AA)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
