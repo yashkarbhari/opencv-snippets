7951b80a3fde9e035cc9cd6071ef9c5a94b2f149
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2020-08-28 15:11:20 +0530

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,22 @@
+import cv2
+
+img = cv2.imread('messi5.jpg')
+img2 = cv2.imread('opencv-logo.png')
+
+print(img.shape) # returns a tuple of no. of rows, colums, and channels
+print(img.size) # returns a total no. of pixels is accessed
+print(img.dtype) # returns Image datatype is obtained
+b, g, r = cv2.split(img)
+img = cv2.merge((b, g, r))
+
+ball = img[280:340, 330:390]
+img[273:333, 100:160] = ball
+
+img = cv2.resize(img, (512, 512))
+img2 = cv2.resize(img2, (512, 512))
+
+dst = cv2.add(img, img2);
+
+cv2.imshow('image', dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
