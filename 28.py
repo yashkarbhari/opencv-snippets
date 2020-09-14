f865efb12ea9854a78b3a7f9a85361acfdd818f6
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2020-09-14 19:53:24 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+img = cv2.imread('messi5.jpg')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+template = cv2.imread('messi_face.jpg', 0)
+w, h = template.shape[::-1]
+
+#cv2.matchTemplate(image, template image, method)
+res = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+print(res)
+threshold = 0.95;
+loc = np.where(res >= threshold)
+print(loc)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
