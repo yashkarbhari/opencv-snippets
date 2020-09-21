3a40680bbd8ea96f350f9a6cec24cdcf0412a820
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2020-09-21 22:24:46 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,1 +1,23 @@
+import cv2
+import numpy as np
+img = cv2.imread('smarties.png')
+output = img.copy()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, ksize=5)
 
+# cv2.HoughCircles(image, method, dp(inverse ratio of accumulator resolution
+# to the image resolution), minDist(min dist. between the centers of the
+# detected circles), param1, param2, minRadius, maxRadius)
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, 
+                           param1=50, param2=30, minRadius=0, maxRadius=0)
+
+detected_circles = np.uint16(np.around(circles))
+for (x, y, r) in detected_circles[0, :]:
+    cv2.circle(output, (x, y), r, (0, 255, 0), 3)
+    cv2.circle(output, (x, y), 2, (0, 255, 255), 3)
+    
+    
+
+cv2.imshow('output', output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
