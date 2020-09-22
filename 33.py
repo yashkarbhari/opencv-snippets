16873263ed0313ad2cf437e739614c8cbac33cb7
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2020-09-23 01:08:41 +0530

    Update 33.py

--- 33.py
+++ 33.py
@@ -1,1 +1,22 @@
+# First, a classifier is trained with a few hundred sample views of a 
+# particular object(i.e., a face or a car), called positive examples,
+# that are scaled to the same size, and negative examples- arbitary 
+# images of the same size.
 
+import cv2
+import numpy as np
+
+# cv2.CascadeClassifier(trained classifier file name)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread('lena.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# cv2.CascadeClassifier.detectMultiScale(image, scaleFactor, minNeighbors)
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 3)
+
+cv2.imshow('img', img)
+cv2.waitKey()
