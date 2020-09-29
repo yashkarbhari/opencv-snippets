2df04d8dd03fbb881bce58950d067e36fc941b5e
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2020-09-29 15:35:42 +0530

    Update 38.py

--- 38.py
+++ 38.py
@@ -23,4 +23,25 @@ cap.release()
 cv2.destroyAllWindows()
 -------------------------------------------------------
 # with BackgroundSubtractorKNN()
+import cv2
+import numpy as np
 
+cap = cv2.VideoCapture('vtest.avi')
+# cv2.bgsegm.createBackgroundSubtractorMOG()
+fgbg = cv2.createBackgroundSubtractorKNN()
+
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    fgmask = fgbg.apply(frame)
+        
+    cv2.imshow('Frame', frame)
+    cv2.imshow('FG MASK Frame', fgmask)
+    
+    keyboard = cv2.waitKey(20)
+    if keyboard == 'q' or keyboard == 27:
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
